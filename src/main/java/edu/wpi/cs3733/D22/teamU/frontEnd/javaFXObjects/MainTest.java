7940296dccc4fc66c46389406da0c6d0bf02d089
Harsh Patel <55161123+hmpatel1350@users.noreply.github.com>
2022-04-13 21:06:05 -0400

    Delete MainTest.java

--- src/main/java/edu/wpi/cs3733/D22/teamU/frontEnd/javaFXObjects/MainTest.java
+++
@@ -1,7 +0,0 @@
-package edu.wpi.cs3733.D22.teamU.frontEnd.javaFXObjects;
-
-public class MainTest {
-  public static void main(String[] args) {
-    ComboBoxAutoCompleteTest.launch(ComboBoxAutoCompleteTest.class, args);
-  }
-}
