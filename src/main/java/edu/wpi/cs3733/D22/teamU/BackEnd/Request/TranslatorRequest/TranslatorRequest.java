3715a59950151cfad4b2a6ad8f7746ca1d578b96
willdoyle757 <61399895+willdoyle757@users.noreply.github.com>
2022-04-18 16:25:04 -0400

    csv file

---
+++ src/main/java/edu/wpi/cs3733/D22/teamU/BackEnd/Request/TranslatorRequest/TranslatorRequest.java
@@ -0,0 +1,28 @@
+package edu.wpi.cs3733.D22.teamU.BackEnd.Request.TranslatorRequest;
+
+import edu.wpi.cs3733.D22.teamU.BackEnd.Employee.Employee;
+import edu.wpi.cs3733.D22.teamU.BackEnd.Request.Request;
+
+public class TranslatorRequest extends Request {
+
+    public String toLang;
+
+    public TranslatorRequest(
+            String ID,
+            String patientName,
+            String toLang,
+            String status,
+            Employee employee,
+            String destination,
+            String date,
+            String time) {
+        this.ID = ID;
+        this.patientName = patientName;
+        this.toLang = toLang;
+        this.status = status;
+        this.employee = employee;
+        this.destination = destination;
+        this.date = date;
+        this.time = time;
+    }
+}
