8de060718103ca1ac84cda68fde691c2c69cd4fa
joselinb <joselinb2001@gmail.com>
2022-04-20 01:20:52 -0400

    Gift and Floral page with working table

--- src/main/java/edu/wpi/cs3733/D22/teamU/frontEnd/controllers/giftFloralController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamU/frontEnd/controllers/giftFloralController.java
@@ -2,6 +2,7 @@ package edu.wpi.cs3733.D22.teamU.frontEnd.controllers;
 
 import com.jfoenix.controls.JFXCheckBox;
 import edu.wpi.cs3733.D22.teamU.BackEnd.Employee.Employee;
+import edu.wpi.cs3733.D22.teamU.BackEnd.Employee.EmployeeDaoImpl;
 import edu.wpi.cs3733.D22.teamU.BackEnd.Location.Location;
 import edu.wpi.cs3733.D22.teamU.BackEnd.Request.GiftRequest.GiftRequest;
 import edu.wpi.cs3733.D22.teamU.BackEnd.Udb;
@@ -9,21 +10,20 @@ import edu.wpi.cs3733.D22.teamU.frontEnd.Uapp;
 import edu.wpi.cs3733.D22.teamU.frontEnd.javaFXObjects.ComboBoxAutoComplete;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
+import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
-import javafx.beans.Observable;
-import javafx.beans.binding.Bindings;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.Tooltip;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -39,16 +39,29 @@ public class giftFloralController extends ServiceController {
   @FXML Button submitButton;
   @FXML VBox requestHolder;
   @FXML Text requestText;
-  @FXML VBox inputFields;
   @FXML StackPane requestsStack;
   @FXML Pane newRequestPane;
   @FXML Pane activeRequestPane;
   @FXML Button newReqButton;
   @FXML Button activeReqButton;
   @FXML Text time;
+  @FXML TableView<GiftRequest> activeRequestTable;
+  @FXML TableColumn<GiftRequest, String> activeReqID;
+  @FXML TableColumn<GiftRequest, String> activeReqName;
+  @FXML TableColumn<GiftRequest, String> activePatientName;
+  @FXML TableColumn<GiftRequest, String> activeGifts;
+  @FXML TableColumn<GiftRequest, String> activeMessage;
+  @FXML TableColumn<GiftRequest, String> activeStatus;
+  @FXML TableColumn<GiftRequest, String> activeEmployee;
+  @FXML TableColumn<GiftRequest, String> activeDestination;
+  @FXML TableColumn<GiftRequest, String> activeDate;
+  @FXML TableColumn<GiftRequest, String> activeTime;
+  @FXML TextField patientName;
+  @FXML TextField senderName;
+  @FXML TextArea message;
 
   ObservableList<JFXCheckBox> checkBoxes = FXCollections.observableArrayList();
-  ObservableList<GiftRequest> giftRequest = FXCollections.observableArrayList();
+  ObservableList<GiftRequest> giftRequests = FXCollections.observableArrayList();
   ArrayList<String> nodeIDs;
   ArrayList<String> staff;
   private static final SimpleDateFormat sdf3 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
@@ -56,18 +69,36 @@ public class giftFloralController extends ServiceController {
   @SneakyThrows
   @Override
   public void initialize(URL location, ResourceBundle resources) {
-    setUpActiveRequests();
+    try {
+      setUpActiveRequests();
+    } catch (SQLException throwables) {
+      throwables.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
     nodeIDs = new ArrayList<>();
-    for (Location l : Udb.getInstance().locationImpl.list()) {
-      nodeIDs.add(l.getNodeID());
+    try {
+      for (Location l : Udb.getInstance().locationImpl.list()) {
+        nodeIDs.add(l.getNodeID());
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (SQLException throwables) {
+      throwables.printStackTrace();
     }
     locations.setTooltip(new Tooltip());
     locations.getItems().addAll(nodeIDs);
     new ComboBoxAutoComplete<String>(locations, 650, 290);
 
     staff = new ArrayList<>();
-    for (Employee l : Udb.getInstance().EmployeeImpl.hList().values()) {
-      staff.add(l.getEmployeeID());
+    try {
+      for (Employee l : Udb.getInstance().EmployeeImpl.hList().values()) {
+        staff.add(l.getEmployeeID());
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (SQLException throwables) {
+      throwables.printStackTrace();
     }
     employees.setTooltip(new Tooltip());
     employees.getItems().addAll(staff);
@@ -77,32 +108,198 @@ public class giftFloralController extends ServiceController {
       checkBoxes.add((JFXCheckBox) checkBox);
     }
 
-    for (int i = 0; i < checkBoxes.size(); i++) {
-      int finalI = i;
-      checkBoxes
-          .get(i)
-          .disableProperty()
-          .bind(
-              Bindings.createBooleanBinding(
-                  () -> !checkBoxes.get(finalI).isSelected(),
-                  checkBoxes.stream().map(CheckBox::selectedProperty).toArray(Observable[]::new)));
-    }
-    clearButton
-        .disableProperty()
-        .bind(
-            Bindings.createBooleanBinding(
-                () -> checkBoxes.stream().noneMatch(JFXCheckBox::isSelected),
-                checkBoxes.stream().map(JFXCheckBox::selectedProperty).toArray(Observable[]::new)));
-
     // BooleanBinding submit =locations.idProperty().isEmpty().and(
     // Bindings.createBooleanBinding(checkBoxes.stream().noneMatch(JFXCheckBox::isSelected)));
-    submitButton
-        .disableProperty()
-        .bind(
-            Bindings.createBooleanBinding(
-                () -> checkBoxes.stream().noneMatch(JFXCheckBox::isSelected),
-                checkBoxes.stream().map(JFXCheckBox::selectedProperty).toArray(Observable[]::new)));
+
     handleTime();
+  }
+
+  private void handleTime() {
+    Thread timeThread =
+        new Thread(
+            () -> {
+              while (Uapp.running) {
+                Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+                String timeStampTime = sdf3.format(timestamp).substring(11);
+                time.setText(timeStampTime);
+              }
+            });
+    timeThread.start();
+  }
+
+  private void setUpActiveRequests() throws SQLException, IOException {
+    activeReqID.setCellValueFactory(new PropertyValueFactory<>("ID"));
+    activeReqName.setCellValueFactory(new PropertyValueFactory<>("name"));
+    activePatientName.setCellValueFactory(new PropertyValueFactory<>("patientName"));
+    activeGifts.setCellValueFactory(new PropertyValueFactory<>("gifts"));
+    activeMessage.setCellValueFactory(new PropertyValueFactory<>("message"));
+    activeStatus.setCellValueFactory(new PropertyValueFactory<>("status"));
+    activeEmployee.setCellValueFactory(new PropertyValueFactory<>("employee"));
+    activeDestination.setCellValueFactory(new PropertyValueFactory<>("destination"));
+    activeDate.setCellValueFactory(new PropertyValueFactory<>("date"));
+    activeTime.setCellValueFactory(new PropertyValueFactory<>("time"));
+    activeRequestTable.setItems(getActiveRequestList());
+  }
+
+  private ObservableList<GiftRequest> newRequest(
+      String ID,
+      String name, // sender name
+      String patientName,
+      String gifts,
+      String message,
+      String status,
+      Employee employee,
+      String destination,
+      String date,
+      String time) {
+    giftRequests.add(
+        new GiftRequest(
+            ID, name, patientName, gifts, message, status, employee, destination, date, time));
+    return giftRequests;
+  }
+
+  private ObservableList<GiftRequest> getActiveRequestList() throws SQLException, IOException {
+    for (GiftRequest giftRequest : Udb.getInstance().giftRequestImpl.hList().values()) {
+      giftRequests.add(
+          new GiftRequest(
+              giftRequest.ID,
+              giftRequest.name,
+              giftRequest.patientName,
+              giftRequest.getGifts(),
+              giftRequest.getMessage(),
+              giftRequest.status,
+              giftRequest.employee,
+              giftRequest.destination,
+              giftRequest.date,
+              giftRequest.time));
+    }
+    return giftRequests;
+  }
+
+  @Override
+  public void addRequest() {
+    StringBuilder startRequestString = new StringBuilder("Your request for : ");
+
+    String endRequest = " has been placed successfully";
+    Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+
+    String inputString = "";
+    for (int i = 0; i < checkBoxes.size(); i++) {
+      if (checkBoxes.get(i).isSelected()) {
+        inputString += checkBoxes.get(i).getText() + ", ";
+      }
+    }
+    String room = locations.getValue().toString();
+
+    String em = (employees.getValue().toString());
+
+    double rand = Math.random() * 10000;
+
+    GiftRequest request =
+        new GiftRequest(
+            (int) rand + "",
+            senderName.getText(),
+            patientName.getText(),
+            message.getText(),
+            inputString,
+            "pending",
+            checkEmployee(em),
+            room,
+            sdf3.format(timestamp).substring(0, 10),
+            sdf3.format(timestamp).substring(11));
+
+    activeRequestTable.setItems(
+        newRequest(
+            request.getID(),
+            request.getName(),
+            request.getPatientName(),
+            request.getGifts(),
+            request.getMessage(),
+            request.getStatus(),
+            request.getEmployee(),
+            request.getDestination(),
+            request.getDate(),
+            request.getTime()));
+    try {
+      Udb.getInstance()
+          .add( // TODO Have random ID and enter Room Destination
+              new GiftRequest(
+                  request.getID(),
+                  request.getName(),
+                  request.getPatientName(),
+                  request.getGifts(),
+                  request.getMessage(),
+                  request.getStatus(),
+                  checkEmployee(employees.getValue().toString()),
+                  request.getDestination(),
+                  request.getDate(),
+                  request.getTime()));
+
+    } catch (IOException e) {
+      e.printStackTrace();
+    } catch (SQLException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void clearRequest() {
+    for (int i = 0; i < checkBoxes.size(); i++) {
+      checkBoxes.get(i).setSelected(false);
+    }
+    requestText.setText("Cleared Requests!");
+    requestText.setVisible(true);
+    new Thread(
+            () -> {
+              try {
+                Thread.sleep(1500); // milliseconds
+                Platform.runLater(
+                    () -> {
+                      message.setText("");
+                      senderName.setText("");
+                      patientName.setText("");
+                    });
+                Thread.sleep(1500); // milliseconds
+                Platform.runLater(
+                    () -> {
+                      requestText.setVisible(false);
+                    });
+              } catch (InterruptedException ie) {
+              }
+            })
+        .start();
+  }
+
+  public Employee checkEmployee(String employee) throws NullPointerException {
+    if (EmployeeDaoImpl.List.get(employee) != null) {
+      return EmployeeDaoImpl.List.get(employee);
+    } else {
+      Employee empty = new Employee("N/A");
+      return empty;
+    }
+  }
+
+  public void switchToNewRequest(ActionEvent actionEvent) {
+    ObservableList<Node> stackNodes = requestsStack.getChildren();
+    Node newReq = stackNodes.get(stackNodes.indexOf(newRequestPane));
+    for (Node node : stackNodes) {
+      node.setVisible(false);
+    }
+    newReq.setVisible(true);
+    newReq.toBack();
+    activeReqButton.setUnderline(false);
+    newReqButton.setUnderline(true);
+  }
+
+  public void switchToActive(ActionEvent actionEvent) {
+    ObservableList<Node> stackNodes = requestsStack.getChildren();
+    Node active = stackNodes.get(stackNodes.indexOf(activeRequestPane));
+    for (Node node : stackNodes) {
+      node.setVisible(false);
+    }
+    active.setVisible(true);
+    active.toBack();
+    activeReqButton.setUnderline(true);
+    newReqButton.setUnderline(false);
   }
 
   public void toHelp(ActionEvent actionEvent) throws IOException {
@@ -111,9 +308,6 @@ public class giftFloralController extends ServiceController {
     appStage.setScene(scene);
     appStage.show();
   }
-
-  @Override
-  public void addRequest() {}
 
   @Override
   public void removeRequest() {}
