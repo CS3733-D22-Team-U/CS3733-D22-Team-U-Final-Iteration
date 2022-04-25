cad533a9d4a62e824f8b7d66982c582f94747c02
willdoyle757 <61399895+willdoyle757@users.noreply.github.com>
2022-04-24 22:47:55 -0400

    fixed medicine request

--- src/main/java/edu/wpi/cs3733/D22/teamU/frontEnd/controllers/EmployeeInfoController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamU/frontEnd/controllers/EmployeeInfoController.java
@@ -1,21 +1,19 @@
 package edu.wpi.cs3733.D22.teamU.frontEnd.controllers;
 
+import edu.wpi.cs3733.D22.teamU.frontEnd.Uapp;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.ResourceBundle;
-
-import edu.wpi.cs3733.D22.teamU.frontEnd.Uapp;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.text.Text;
 
 public class EmployeeInfoController extends ServiceController {
 
-  @FXML
-  Text time;
+  @FXML Text time;
   @FXML Text date;
 
   private static final SimpleDateFormat sdf3 = new SimpleDateFormat("MM-dd-yyyy HH:mm:ss");
@@ -27,16 +25,16 @@ public class EmployeeInfoController extends ServiceController {
 
   private void handeDateTime() {
     Thread timeThread =
-            new Thread(
-                    () -> {
-                      while (Uapp.running) {
-                        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-                        String timeStampdate = sdf3.format(timestamp).substring(0, 10);
-                        String timeStampTime = sdf3.format(timestamp).substring(11);
-                        time.setText(timeStampTime);
-                        date.setText(timeStampdate);
-                      }
-                    });
+        new Thread(
+            () -> {
+              while (Uapp.running) {
+                Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+                String timeStampdate = sdf3.format(timestamp).substring(0, 10);
+                String timeStampTime = sdf3.format(timestamp).substring(11);
+                time.setText(timeStampTime);
+                date.setText(timeStampdate);
+              }
+            });
     timeThread.start();
   }
 
