f2937bfcf5fbd78fb37465b8b8936b98d95791ed
joselinb <joselinb2001@gmail.com>
2022-04-27 16:43:30 -0400

    added new UI to employee, report and all request page

--- src/main/java/edu/wpi/cs3733/D22/teamU/frontEnd/controllers/AboutController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamU/frontEnd/controllers/AboutController.java
@@ -1,26 +1,18 @@
 package edu.wpi.cs3733.D22.teamU.frontEnd.controllers;
 
 import com.jfoenix.controls.JFXHamburger;
-
+import edu.wpi.cs3733.D22.teamU.frontEnd.Uapp;
 import java.awt.*;
 import java.net.URL;
-import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
-import java.time.LocalDate;
-import java.util.Objects;
 import java.util.ResourceBundle;
-
-import edu.wpi.cs3733.D22.teamU.frontEnd.Uapp;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.Node;
 import javafx.scene.control.DatePicker;
-import javafx.scene.control.skin.DatePickerSkin;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.text.Text;
-
 
 public class AboutController extends ServiceController {
 
@@ -30,17 +22,12 @@ public class AboutController extends ServiceController {
   @FXML JFXHamburger hamburger;
   @FXML ImageView imageHover;
   private double x, y;
-  @FXML
-  TextField quote;
+  @FXML TextField quote;
 
   @FXML DatePicker datePicker;
   @FXML Pane datePickerPane;
-  @FXML
-  javafx.scene.text.Text time;
-  @FXML
-  Text date;
-
-
+  @FXML javafx.scene.text.Text time;
+  @FXML Text date;
 
   @Override
   public void addRequest() {}
@@ -52,55 +39,59 @@ public class AboutController extends ServiceController {
   public void updateRequest() {}
 
   @Override
-  public void initialize(URL location, ResourceBundle resources) {
+  public void initialize(URL location, ResourceBundle resources) {}
 
-  }
-
-
-
-  public void toHarsh(){
+  public void toHarsh() {
     addTeamImage("edu/wpi/cs3733/D22/teamU/images/groupMemberPics/harshRESIZE.jpeg");
   }
-  public void toMarko(ActionEvent actionEvent){
-    addTeamImage("edu/wpi/cs3733/D22/teamU/images/groupMemberPics/markoRESIZE.jpeg");
 
+  public void toMarko(ActionEvent actionEvent) {
+    addTeamImage("edu/wpi/cs3733/D22/teamU/images/groupMemberPics/markoRESIZE.jpeg");
   }
-  public void toJoselin(ActionEvent actionEvent){
+
+  public void toJoselin(ActionEvent actionEvent) {
     addTeamImage("edu/wpi/cs3733/D22/teamU/images/groupMemberPics/joselin2RESIZE.jpeg");
   }
-  public void toDeepti(ActionEvent actionEvent){
+
+  public void toDeepti(ActionEvent actionEvent) {
     addTeamImage("edu/wpi/cs3733/D22/teamU/images/groupMemberPics/deeptiRESIZE.jpeg");
   }
-  public void toNick(ActionEvent actionEvent){
+
+  public void toNick(ActionEvent actionEvent) {
     addTeamImage("edu/wpi/cs3733/D22/teamU/images/groupMemberPics/nickRESIZE.jpeg");
   }
-  public void toKody(ActionEvent actionEvent){
+
+  public void toKody(ActionEvent actionEvent) {
     addTeamImage("edu/wpi/cs3733/D22/teamU/images/groupMemberPics/kodyRESIZE.jpeg");
   }
-  public void toTim(ActionEvent actionEvent){
+
+  public void toTim(ActionEvent actionEvent) {
     addTeamImage("edu/wpi/cs3733/D22/teamU/images/groupMemberPics/timRESIZE.jpeg");
   }
-  public void toWill(ActionEvent actionEvent){
+
+  public void toWill(ActionEvent actionEvent) {
     addTeamImage("edu/wpi/cs3733/D22/teamU/images/groupMemberPics/willRESIZE.jpeg");
   }
-  public void toMike(ActionEvent actionEvent){
+
+  public void toMike(ActionEvent actionEvent) {
     addTeamImage("edu/wpi/cs3733/D22/teamU/images/groupMemberPics/mikeRESIZE.jpeg");
   }
-  public void toIain(ActionEvent actionEvent){
+
+  public void toIain(ActionEvent actionEvent) {
     addTeamImage("edu/wpi/cs3733/D22/teamU/images/groupMemberPics/iainRESIZE.jpeg");
   }
-  public void toBelisha(ActionEvent actionEvent){
+
+  public void toBelisha(ActionEvent actionEvent) {
     addTeamImage("edu/wpi/cs3733/D22/teamU/images/groupMemberPics/belishaRESIZE.jpeg");
   }
 
   private void addTeamImage(String resource) {
     URL a = Uapp.class.getClassLoader().getResource(resource);
     imageHover.setImage(new Image(String.valueOf(a)));
-    //imageHover.setScaleX(1.75);
-    //imageHover.setScaleY(1.75);
-    //imageHover.setX((imageHover.getFitWidth()));
-    //imageHover.setY((imageHover.getFitHeight()));
+    // imageHover.setScaleX(1.75);
+    // imageHover.setScaleY(1.75);
+    // imageHover.setX((imageHover.getFitWidth()));
+    // imageHover.setY((imageHover.getFitHeight()));
 
   }
 }
-
