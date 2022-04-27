65fbe40839ce2c7db250d877963a4a07defbd01e
Harsh Patel <55161123+hmpatel1350@users.noreply.github.com>
2022-04-27 09:29:27 -0400

    Update LocationDaoImpl.java
    Commented out Firebase update

--- src/main/java/edu/wpi/cs3733/D22/teamU/BackEnd/Location/LocationDaoImpl.java
+++ src/main/java/edu/wpi/cs3733/D22/teamU/BackEnd/Location/LocationDaoImpl.java
@@ -104,6 +104,7 @@ public class LocationDaoImpl implements DataDao<Location> {
 
       for (int j = 0; j < locations.size(); j++) {
         Location currLoc = locations.get(j);
+        /*
         try {
           firebaseUpdate(currLoc);
         } catch (ExecutionException e) {
@@ -111,6 +112,7 @@ public class LocationDaoImpl implements DataDao<Location> {
         } catch (InterruptedException e) {
           throw new RuntimeException(e);
         }
+        */
         statement.execute(
             "INSERT INTO Locations VALUES("
                 + "'"
