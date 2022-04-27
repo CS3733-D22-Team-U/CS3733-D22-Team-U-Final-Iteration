2fce5da6b0f2be17d3575086381c111f3d46dcaf
krob <kodyrobinson42>
2022-04-27 02:00:18 -0400

    GOT THE IMAGES TO WORK ON HOVER, WORKING ON ADDING QUOTES ON HOVER SO IT IS MORE USER INTUITIVE AND SWAGGER FUNNY YAY

--- src/main/java/edu/wpi/cs3733/D22/teamU/frontEnd/controllers/AboutController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamU/frontEnd/controllers/AboutController.java
@@ -2,14 +2,24 @@ package edu.wpi.cs3733.D22.teamU.frontEnd.controllers;
 
 import com.jfoenix.controls.JFXHamburger;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
+
+import edu.wpi.cs3733.D22.teamU.frontEnd.Uapp;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 
 public class AboutController extends ServiceController {
 
   @FXML Pane pane;
   @FXML JFXHamburger hamburger;
+  @FXML ImageView imageHover;
+  private double x, y;
+
+
 
   @Override
   public void addRequest() {}
@@ -22,4 +32,52 @@ public class AboutController extends ServiceController {
 
   @Override
   public void initialize(URL location, ResourceBundle resources) {}
+
+
+  public void toHarsh(){
+    addTeamImage("edu/wpi/cs3733/D22/teamU/images/groupMemberPics/HarshIcon.jpeg");
+  }
+  public void toMarko(ActionEvent actionEvent){
+    addTeamImage("edu/wpi/cs3733/D22/teamU/images/groupMemberPics/MarkoIcon.jpeg");
+  }
+  public void toJoselin(ActionEvent actionEvent){
+    addTeamImage("edu/wpi/cs3733/D22/teamU/images/groupMemberPics/JoselinIcon.jpeg");
+  }
+  public void toDeepti(ActionEvent actionEvent){
+    addTeamImage("edu/wpi/cs3733/D22/teamU/images/groupMemberPics/DeeptiIcon.jpeg");
+  }
+  public void toNick(ActionEvent actionEvent){
+    addTeamImage("edu/wpi/cs3733/D22/teamU/images/groupMemberPics/NickIcon.jpeg");
+  }
+  public void toKody(ActionEvent actionEvent){
+    addTeamImage("edu/wpi/cs3733/D22/teamU/images/groupMemberPics/KodyIcon.jpeg");
+  }
+  public void toTim(ActionEvent actionEvent){
+    addTeamImage("edu/wpi/cs3733/D22/teamU/images/groupMemberPics/TimIcon.jpeg");
+  }
+  public void toWill(ActionEvent actionEvent){
+    addTeamImage("edu/wpi/cs3733/D22/teamU/images/groupMemberPics/WillIcon.jpeg");
+  }
+  public void toMike(ActionEvent actionEvent){
+    addTeamImage("edu/wpi/cs3733/D22/teamU/images/groupMemberPics/MikeIcon.jpeg");
+  }
+  public void toIain(ActionEvent actionEvent){
+    addTeamImage("edu/wpi/cs3733/D22/teamU/images/groupMemberPics/IAINIcon.jpeg");
+  }
+  public void toBelisha(ActionEvent actionEvent){
+    addTeamImage("edu/wpi/cs3733/D22/teamU/images/groupMemberPics/BelishaIcon.jpeg");
+  }
+
+  private void addTeamImage(String resource) {
+    URL a = Uapp.class.getClassLoader().getResource(resource);
+    imageHover.setImage(new Image(String.valueOf(a)));
+    imageHover.setScaleX(imageHover.getScaleX());
+    imageHover.setScaleY(imageHover.getScaleY());
+    imageHover.setFitHeight(imageHover.getFitWidth());
+    imageHover.setFitWidth(imageHover.getFitWidth());
+    //imageHover.setX((imageHover.getFitWidth()));
+    //imageHover.setY((imageHover.getFitHeight()));
+    imageHover.setScaleX(1.15);
+    imageHover.setScaleY(1.15);
+  }
 }
