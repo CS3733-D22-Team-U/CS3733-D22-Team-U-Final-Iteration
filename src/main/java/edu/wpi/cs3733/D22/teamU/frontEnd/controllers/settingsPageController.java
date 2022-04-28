aa2c01a7e45317d95f80bd1cca7ede851199a51c
Marko <mvila0119@gmail.com>
2022-04-27 20:21:05 -0400

    changed all the edit request buttons to be unifrom

--- src/main/java/edu/wpi/cs3733/D22/teamU/frontEnd/controllers/settingsPageController.java
+++ src/main/java/edu/wpi/cs3733/D22/teamU/frontEnd/controllers/settingsPageController.java
@@ -3,14 +3,12 @@ package edu.wpi.cs3733.D22.teamU.frontEnd.controllers;
 import edu.wpi.cs3733.D22.teamU.BackEnd.Employee.Employee;
 import edu.wpi.cs3733.D22.teamU.BackEnd.Udb;
 import edu.wpi.cs3733.D22.teamU.frontEnd.Uapp;
-
 import java.awt.*;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
-
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -19,12 +17,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
-import javafx.scene.control.TextField;
-
-
 
 public class settingsPageController implements Initializable {
 
@@ -50,8 +46,6 @@ public class settingsPageController implements Initializable {
     comboImages.setItems(URLList);
   }
 
-
-
   public void toApplicationSettings(ActionEvent actionEvent) throws IOException {
     Scene scene = Uapp.getScene("edu/wpi/cs3733/D22/teamU/views/applicationSettings.fxml");
     Stage appStage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
@@ -59,41 +53,32 @@ public class settingsPageController implements Initializable {
     appStage.show();
   }
 
-  public void changeImage(ActionEvent actionEvent) throws IOException{
-    if(comboImages.getValue().equals("Belisha Image")){
+  public void changeImage(ActionEvent actionEvent) throws IOException {
+    if (comboImages.getValue().equals("Belisha Image")) {
       addTeamImage("edu/wpi/cs3733/D22/teamU/images/groupMemberPics/belishaRESIZE.jpeg");
-    }
-    else if(comboImages.getValue().equals("Deepti Image")){
+    } else if (comboImages.getValue().equals("Deepti Image")) {
       addTeamImage("edu/wpi/cs3733/D22/teamU/images/groupMemberPics/deeptiRESIZE.jpeg");
-    }
-    else if(comboImages.getValue().equals("Harsh Image")){
+    } else if (comboImages.getValue().equals("Harsh Image")) {
       addTeamImage("edu/wpi/cs3733/D22/teamU/images/groupMemberPics/harshRESIZE.jpeg");
-    }
-    else if(comboImages.getValue().equals("Iain Image")){
+    } else if (comboImages.getValue().equals("Iain Image")) {
       addTeamImage("edu/wpi/cs3733/D22/teamU/images/groupMemberPics/iainRESIZE.jpeg");
-    }
-    else if(comboImages.getValue().equals("Joselin Image")){
+    } else if (comboImages.getValue().equals("Joselin Image")) {
       addTeamImage("edu/wpi/cs3733/D22/teamU/images/groupMemberPics/joselin2RESIZE.jpeg");
-    }
-    else if(comboImages.getValue().equals("Kody Image")){
+    } else if (comboImages.getValue().equals("Kody Image")) {
       addTeamImage("edu/wpi/cs3733/D22/teamU/images/groupMemberPics/kodyRESIZE.jpeg");
-    }
-    else if(comboImages.getValue().equals("Marko Image")){
+    } else if (comboImages.getValue().equals("Marko Image")) {
       addTeamImage("edu/wpi/cs3733/D22/teamU/images/groupMemberPics/markoRESIZE.jpeg");
-    }
-    else if(comboImages.getValue().equals("Mike Image")){
+    } else if (comboImages.getValue().equals("Mike Image")) {
       addTeamImage("edu/wpi/cs3733/D22/teamU/images/groupMemberPics/mikeRESIZE.jpeg");
-    }
-    else if(comboImages.getValue().equals("Nick Image")){
+    } else if (comboImages.getValue().equals("Nick Image")) {
       addTeamImage("edu/wpi/cs3733/D22/teamU/images/groupMemberPics/nickRESIZE.jpeg");
-    }
-    else if(comboImages.getValue().equals("Tim Image")){
+    } else if (comboImages.getValue().equals("Tim Image")) {
       addTeamImage("edu/wpi/cs3733/D22/teamU/images/groupMemberPics/timRESIZE.jpeg");
-    }
-    else if(comboImages.getValue().equals("Will Image")){
+    } else if (comboImages.getValue().equals("Will Image")) {
       addTeamImage("edu/wpi/cs3733/D22/teamU/images/groupMemberPics/willRESIZE.jpeg");
     }
   }
+
   private void addTeamImage(String resource) {
     URL a = Uapp.class.getClassLoader().getResource(resource);
     changeImage.setImage(new Image(String.valueOf(a)));
@@ -104,9 +89,9 @@ public class settingsPageController implements Initializable {
 
   }
 
-  public void changeName(ActionEvent actionEvent) throws IOException{
+  public void changeName(ActionEvent actionEvent) throws IOException {
     Employee user;
-    try{
+    try {
       user = Udb.getInstance().getUser();
     } catch (SQLException e) {
       throw new RuntimeException(e);
@@ -117,7 +102,7 @@ public class settingsPageController implements Initializable {
     user.setFirstName(newFirst);
     changeText.clear();
 
-    //possibly add so username saves to CSV?
+    // possibly add so username saves to CSV?
   }
 
   public void toHome(ActionEvent actionEvent) throws IOException {
